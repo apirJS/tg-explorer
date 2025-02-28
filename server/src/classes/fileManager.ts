d65b76610e8778f0a653748ceef541908a66a2bd
Echa Apriliyanto <apriliyantoecha1@gmail.com>
2025-02-28 22:48:52 +0700

    feat(server): split file functionality on FileManager

--- server/src/classes/fileManager.ts
+++ server/src/classes/fileManager.ts
@@ -6,60 +6,55 @@ import { createReadStream, createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
 import { join } from 'path';
 import crypto from 'crypto';
+import { formatErrorMessage } from '../lib/utils';
 
 class FileManager {
   private _TWO_GIGABYTES = 2_147_483_648;
-  private _CHUNK_SIZE = 1024 * 1024; // 1MB
+
   constructor() {}
 
-  private async splitFile(filePath: string, outputDir: string) {
-    const readStream = createReadStream(filePath, {
-      highWaterMark: this._CHUNK_SIZE,
-    });
-    let index = 0;
-    let hash = crypto.createHash('sha256');
+  private async splitFile(
+    filePath: string,
+    outputDir: string,
+    chunkSize: number = this._TWO_GIGABYTES
+  ) {
+    try {
+      if (chunkSize > this._TWO_GIGABYTES) {
+        throw new Error(
+          "Chunk size can't be bigger than 2GB due to Telegram limit"
+        );
+      }
 
-    for await (const chunk of readStream) {
-      const chunkPath = join(outputDir, `chunk_${index}.part`);
-      await Bun.write(chunkPath, chunk);
-      hash.update(chunk);
-      index++;
+      const readStream = createReadStream(filePath, {
+        highWaterMark: chunkSize,
+      }) as AsyncIterable<Buffer>;
+      const hash = crypto.createHash('sha256');
+      const fileBasename = path.basename(filePath).slice(0, 246);
+      let index = 0;
+
+      for await (const chunk of readStream) {
+        const chunkPath = path.join(outputDir, `${fileBasename}_${index}.part`);
+        const uint8Array = new Uint8Array(chunk);
+        await Bun.write(chunkPath, uint8Array);
+        hash.update(uint8Array);
+        index++;
+      }
+
+      const checksum = hash.digest('hex');
+      await Bun.write(
+        path.join(outputDir, `${fileBasename}_checksum.txt`),
+        checksum
+      );
+    } catch (error) {
+      throw new Error(formatErrorMessage('Failed to split the file', error));
     }
-
-    const checksum = hash.digest('hex');
-    await Bun.write(join(outputDir, 'checksum.txt'), checksum);
   }
 
   private async mergeFile(outputDir: string, outputFilePath: string) {
-    const files = (await readdir(outputDir))
-      .filter((file) => file.startsWith('chunk_'))
-      .sort((a, b) => parseInt(a.split('_')[1]) - parseInt(b.split('_')[1]));
-
-    const writeStream = createWriteStream(outputFilePath);
-    let hash = crypto.createHash('sha256');
-
-    for (const file of files) {
-      const chunkPath = join(outputDir, file);
-      const chunk = await Bun.file(chunkPath).arrayBuffer();
-      const buffer = Buffer.from(chunk);
-      const uint8Array = new Uint8Array(buffer.buffer);
-      hash.update(uint8Array);
-      writeStream.write(uint8Array);
-    }
-
-    writeStream.end();
-
-    // Verify integrity
-    const originalChecksum = readFileSync(
-      join(outputDir, 'checksum.txt'),
-      'utf8'
-    ).trim();
-    const mergedChecksum = hash.digest('hex');
-
-    if (originalChecksum === mergedChecksum) {
-      //
-    } else {
-      //
+    try {
+      
+    } catch (error) {
+      
     }
   }
 }
